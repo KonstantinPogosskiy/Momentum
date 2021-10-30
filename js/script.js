15d5c803468a46959e956f90a000fb29f6ed56b4
KonstantinPogosskiy <darcus666@yandex.ru>
2021-10-30 16:20:34 +0300

    refactor: add date

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
+//Show time
 const time = document.querySelector('.time');
 
-
 time.textContent = '';
-
 
 function showTime() {
     const date = new Date();
@@ -11,3 +10,16 @@ function showTime() {
     setTimeout(showTime, 1000)
 }
 showTime()
+
+//Show date
+const date = document.querySelector ('.date');
+
+date.textContent = '';
+
+function showDate() {
+    const localDate = new Date();
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
+    const currentDate = localDate.toLocaleDateString('en-US',options);
+    date.textContent = currentDate;
+}
+showDate()
