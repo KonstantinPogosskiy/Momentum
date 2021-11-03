3f33e64c2fc65a9ba45fc4d7a2c3d16f7a8d7745
KonstantinPogosskiy <darcus666@yandex.ru>
2021-11-04 00:01:37 +0300

    refactor: add local storage

--- js/script.js
+++ js/script.js
@@ -43,4 +43,19 @@ function getTimeOfDay(param) {
     }
 }
 
+//Entered and save message
+const name = document.querySelector('.name');
+
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        name.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage)
+
 showTime()
